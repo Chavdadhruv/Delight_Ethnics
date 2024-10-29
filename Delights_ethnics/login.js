03b7765ef088fe17932313d93e675d7536ac0840
Chavda_Dhruv447 <149055293+Chavdadhruv@users.noreply.github.com>
2024-10-29 22:00:33 +0530

    Add files via upload

---
+++ Delights_ethnics/login.js
@@ -0,0 +1,5 @@
+let doctitle = document.title;
+window.addEventListener("blur", () => {
+  document.title = "👋 Hey! come back...";});
+window.addEventListener("focus", () => {
+  document.title=doctitle;});
